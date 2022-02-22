b8011e61bfc2d99209980ca2dd0f86a81b8712d8
Your Name <jazzysingh227@gmail.com>
2022-02-22 18:58:44 +0530

    integrated new filters

--- src/components/filter/index.tsx
+++ src/components/filter/index.tsx
@@ -10,6 +10,8 @@ interface FilterProps {
 
 const FilterForm = () => {
   const [value, setValue] = useState('');
+  const [min, setMin] = useState('');
+  const [max, setMax] = useState('');
   const [data, setData] = React.useState<FilterProps[]>([]);
 
   const filterData = async (e: any) => {
@@ -24,6 +26,43 @@ const FilterForm = () => {
     }
   };
 
+  const filterByLimit = async (e: any) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get(
+        `http://localhost:4000/product/filter/limit?price=${value}`
+      );
+      setData(response.data.result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const filterByLeast = async (e: any) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get(
+        `http://localhost:4000/product/filter/least?price=${value}`
+      );
+      setData(response.data.response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const filterByRange = async (e: any) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get(
+        `http://localhost:4000/product/filter/range?minPrice=${min}&maxPrice=${max}`
+      );
+      setData(response.data.result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log(data);
+
   return (
     <div>
       <form onSubmit={filterData}>
@@ -34,6 +73,62 @@ const FilterForm = () => {
             placeholder="Search By Price"
             onChange={(e: any) => {
               setValue(e.target.value);
+            }}
+          ></input>
+        </div>
+
+        <br></br>
+
+        <button type="submit">Search</button>
+      </form>
+
+      <br></br>
+
+      <form onSubmit={filterByLimit}>
+        <div>
+          <label>Minimum Price:</label>
+          <input
+            type="number"
+            placeholder="Set minimum price"
+            onChange={(e: any) => {
+              setValue(e.target.value);
+            }}
+          ></input>
+        </div>
+        <br></br>
+        <button type="submit">Search</button>
+      </form>
+
+      <br></br>
+      <form onSubmit={filterByLeast}>
+        <div>
+          <label>Maximum Price:</label>
+          <input
+            type="number"
+            placeholder="Set maximum price"
+            onChange={(e: any) => {
+              setValue(e.target.value);
+            }}
+          ></input>
+        </div>
+
+        <br></br>
+
+        <button type="submit">Search</button>
+      </form>
+      <br></br>
+      <form onSubmit={filterByRange}>
+        <div>
+          <input
+            type="number"
+            onChange={(e: any) => {
+              setMin(e.target.value);
+            }}
+          ></input>
+          <input
+            type="number"
+            onChange={(e: any) => {
+              setMax(e.target.value);
             }}
           ></input>
         </div>
